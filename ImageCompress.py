41ae2fb7848dc24de49798c400a49d4e1c3bf754
theuitown <nitingoswami473@gmail.com>
2019-12-30 20:26:52 +0530

    fixed png,jpg compression and added support for linux :)

--- ImageCompress.py
+++ ImageCompress.py
@@ -1,7 +1,7 @@
 import sys
 from termcolor import colored
 from colorama import init
-init(strip=not sys.stdout.isatty()) 
+init(strip=not sys.stdout.isatty())
 from pyfiglet import figlet_format
 from PIL import Image
 import os
@@ -15,44 +15,44 @@ import glob
 
 print(colored("""
 
- ▄████▄   ▒█████   ███▄ ▄███▓ ██▓███   ██▀███  ▓█████   ██████   ██████     ██▓ ███▄ ▄███▓ ▄▄▄        ▄████ ▓█████ 
-▒██▀ ▀█  ▒██▒  ██▒▓██▒▀█▀ ██▒▓██░  ██▒▓██ ▒ ██▒▓█   ▀ ▒██    ▒ ▒██    ▒    ▓██▒▓██▒▀█▀ ██▒▒████▄     ██▒ ▀█▒▓█   ▀ 
-▒▓█    ▄ ▒██░  ██▒▓██    ▓██░▓██░ ██▓▒▓██ ░▄█ ▒▒███   ░ ▓██▄   ░ ▓██▄      ▒██▒▓██    ▓██░▒██  ▀█▄  ▒██░▄▄▄░▒███   
-▒▓▓▄ ▄██▒▒██   ██░▒██    ▒██ ▒██▄█▓▒ ▒▒██▀▀█▄  ▒▓█  ▄   ▒   ██▒  ▒   ██▒   ░██░▒██    ▒██ ░██▄▄▄▄██ ░▓█  ██▓▒▓█  ▄ 
+ ▄████▄   ▒█████   ███▄ ▄███▓ ██▓███   ██▀███  ▓█████   ██████   ██████     ██▓ ███▄ ▄███▓ ▄▄▄        ▄████ ▓█████
+▒██▀ ▀█  ▒██▒  ██▒▓██▒▀█▀ ██▒▓██░  ██▒▓██ ▒ ██▒▓█   ▀ ▒██    ▒ ▒██    ▒    ▓██▒▓██▒▀█▀ ██▒▒████▄     ██▒ ▀█▒▓█   ▀
+▒▓█    ▄ ▒██░  ██▒▓██    ▓██░▓██░ ██▓▒▓██ ░▄█ ▒▒███   ░ ▓██▄   ░ ▓██▄      ▒██▒▓██    ▓██░▒██  ▀█▄  ▒██░▄▄▄░▒███
+▒▓▓▄ ▄██▒▒██   ██░▒██    ▒██ ▒██▄█▓▒ ▒▒██▀▀█▄  ▒▓█  ▄   ▒   ██▒  ▒   ██▒   ░██░▒██    ▒██ ░██▄▄▄▄██ ░▓█  ██▓▒▓█  ▄
 ▒ ▓███▀ ░░ ████▓▒░▒██▒   ░██▒▒██▒ ░  ░░██▓ ▒██▒░▒████▒▒██████▒▒▒██████▒▒   ░██░▒██▒   ░██▒ ▓█   ▓██▒░▒▓███▀▒░▒████▒
 ░ ░▒ ▒  ░░ ▒░▒░▒░ ░ ▒░   ░  ░▒▓▒░ ░  ░░ ▒▓ ░▒▓░░░ ▒░ ░▒ ▒▓▒ ▒ ░▒ ▒▓▒ ▒ ░   ░▓  ░ ▒░   ░  ░ ▒▒   ▓▒█░ ░▒   ▒ ░░ ▒░ ░
   ░  ▒     ░ ▒ ▒░ ░  ░      ░░▒ ░       ░▒ ░ ▒░ ░ ░  ░░ ░▒  ░ ░░ ░▒  ░ ░    ▒ ░░  ░      ░  ▒   ▒▒ ░  ░   ░  ░ ░  ░
-░        ░ ░ ░ ▒  ░      ░   ░░         ░░   ░    ░   ░  ░  ░  ░  ░  ░      ▒ ░░      ░     ░   ▒   ░ ░   ░    ░   
-░ ░          ░ ░         ░               ░        ░  ░      ░        ░      ░         ░         ░  ░      ░    ░ 
+░        ░ ░ ░ ▒  ░      ░   ░░         ░░   ░    ░   ░  ░  ░  ░  ░  ░      ▒ ░░      ░     ░   ▒   ░ ░   ░    ░
+░ ░          ░ ░         ░               ░        ░  ░      ░        ░      ░         ░         ░  ░      ░    ░
 """,'green'))
-print(colored("#Created by KarthikSB", 'white', 'on_green'))
-
+#don't touch the line below :) !!
+print(colored("#Created by KarthikSB & iamharsh.dev", 'white', 'on_green'))
+print('\n')
 
 #importing location
-print("\n")
 imageDir= input(colored(" Enter the location of image folder  ",'green'))
 
 if imageDir.endswith("/"):
     imageDir.replace("/","")
 
-path =glob.glob(imageDir+"/*jpg")
-
-
-#expoting location
+path =glob.glob(imageDir+'/'+"*")
 print("\n")
 
+#expoting location
 outdir =input(colored(" Enter where to Export ",'green'))
 if outdir.endswith("/"):
     outdir.replace("/","")
 
-def compressImage(path,outdir):
-    for im in path:
+
+#magic happens :)
+def compressImage(path,outdir): #function
+    for im in path: #for loop to get all images inside a folder lol
         filename =os.path.basename(im)
         im =Image.open(im)
         im.resize((10,10))
         print(im.format, im.size, im.mode)
-
         im.save(outdir+"/"+filename,quality=50,optimize=True)
 
-compressImage(path,outdir)
-
+compressImage(path,outdir) # calling function
+print(colored("#Created by KarthikSB & iamharsh.dev", 'white', 'on_green'))
+print(colored("#LOL BYE#", 'white', 'on_green'))
